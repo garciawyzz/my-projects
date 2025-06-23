1c68341d6fbbf2457d1905c3a5503c95ebede218
garcia <162590888+garciawyzz@users.noreply.github.com>
2025-06-23 18:37:02 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["a Computer Science student.", "a Developer.", "an Entrepreneur.", "a Trojan."],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+})
